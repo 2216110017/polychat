2a5fd49caa761079bca325be5f1b8b238099cb3e
thistimleose <nightrunfun12@gmail.com>
2023-05-07 14:23:13 +0900

    commit init

---
+++ app/src/main/java/com/example/polychat24/mainActivity.java
@@ -0,0 +1,101 @@
+package com.example.polychat24;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class mainActivity extends AppCompatActivity {
+
+    private DatabaseReference myRef;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    myRef = database.getReference("message");
+
+    myRef.addChildEventListener(new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            Chat chat = snapshot.getValue(Chat.class);
+            ((ChatAdapter)adapter).addChat(chat);
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    });
+
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Chat> chatList;
+    private String nickname = "익명1";
+
+    private EditText chatText;
+    private Button sendButton;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chatText = findViewById(R.id.chatText);
+        sendButton = findViewById(R.id.sendButton);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = chatText.getText().toString();
+
+                if(msg != null){
+                    Chat chat = new Chat();
+                    chat.setName(nickname);
+                    chat.setMsg(msg);
+
+                    chatText.setText("");
+                }
+
+            }
+        });
+
+        recyclerView = findViewById(R.id.user_recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        chatList = new ArrayList<>();
+        adapter = new ChatAdapter(chatList, nickname);
+        recyclerView.setAdapter(adapter);
+
+    }
+}
